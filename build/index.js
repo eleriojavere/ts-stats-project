50f0526d52fe088be85392dbbdefcd309b3ec2de
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-05-30 21:05:06 +0300

    Add a class that reads and returns csv file content

--- build/index.js
+++ build/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('hello');
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const matchResult_1 = require("./matchResult");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+const dateOfFirstMatch = reader.data[0][0];
+console.log(dateOfFirstMatch);
+let manchesterUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === matchResult_1.MatchResult.HomeWin) {
+        manchesterUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === matchResult_1.MatchResult.AwayWin) {
+        manchesterUnitedWins++;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello');
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './matchResult';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+const dateOfFirstMatch = reader.data[0][0];
+console.log(dateOfFirstMatch);
+
+let manchesterUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manchesterUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manchesterUnitedWins++;
+  }
+}
